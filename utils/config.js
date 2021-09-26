847e377f9e68462a6fea559e4368212a1cb6b073
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-27 04:09:23 +0800

    Delete utils directory

--- utils/config.js
+++
@@ -1,7 +0,0 @@
-const dev = process.env.NODE_ENV !== 'production';
-
-export const server = dev ? 'http://localhost:3000' : 'https://developer-portfolio-dun.vercel.app';
-
-export const mediumUserName = 'korayguler';
-
-export const avatar = '/images/avatar.jpg';
